6938bc35dda792fd438b8c5834f1d5f80ce0a81d
wbeh501 <weiten_beh@comcast.com>
2021-04-13 15:14:32 -0400

    updated presto_connect to include auth with pem file

--- data_access/presto_connect.py
+++ data_access/presto_connect.py
@@ -1,38 +1,84 @@
+# Reference: https://etwiki.sys.comcast.net/display/DX/Python+Clients
+
 import prestodb
 from getpass import getpass
+
 # import query_builder
 
 
-def get_presto_conn(host, port, user, catalog, schema):
-    conn=prestodb.dbapi.connect(
+server = {
+    "host": "query.comcast.com",
+    "port": 4443,
+    "ca_bundle": "./query.comcast.com.pem",
+}
+
+user_credentials = {
+    "user": getpass.getuser(),
+    "pass": getpass("password:"),  # TODO: verify if password can be retrieved properly
+}
+
+
+class enhancedAuth(presto.auth.BasicAuthentication):
+    def __init__(
+        self,
+        username,
+        password,
+        ca_bundle=None,  # type: Optional[Text]
+    ):
+        super().__init__(username, password)
+        self._ca_bundle = ca_bundle
+
+    def set_http_session(self, http_session):
+        http_session = super().set_http_session(http_session)
+        if self._ca_bundle:
+            http_session.verify = self._ca_bundle
+        return http_session
+
+
+def get_presto_conn(host, port, user, catalog, schema, http_scheme, auth):
+    conn = prestodb.dbapi.connect(
         host=host,
         port=port,
         user=user,
         catalog=catalog,
         schema=schema,
+        http_scheme=http_scheme,
+        auth=auth,
     )
     return conn
 
-conn = get_presto_conn(
-    host='query.comcast.com',
-    port=4443,
-    user='dmirza509',
-    catalog='test_catalog',
-    schema='test_schema',
-    http_scheme='https',
-    auth=prestodb.auth.BasicAuthentication("dmirza509", getpass("password:"))
+
+if __name__ == "__main__":
+    # authenticate
+    _auth = enhancedAuth(
+        username=user_credentials["user"],
+        password=user_credentials["pass"],
+        ca_bundle=server["ca_bundle"],
     )
-cur = conn.cursor()
 
-try:
-    # TODO: get query from query_builder
-    query = "SELECT * FROM test_db"
+    # connect to presto db
+    conn = get_presto_conn(
+        host=server["host"],
+        port=server["port"],
+        user=user_credentials["user"],
+        catalog="test_catalog",
+        schema="test_schema",
+        http_scheme="https",
+        auth=_auth,
+    )
+    cur = conn.cursor()
 
-    cur.execute(query)
-    rows = cur.fetchall()
+    # query presto db
+    try:
+        # TODO: get query from query_builder
+        query = "SELECT * FROM test_db"
 
-finally:
-    cur.close()
-    conn.close()
+        cur.execute(query)
+        rows = cur.fetchall()
 
-# TODO: return rows to react application for visuals
+    finally:
+        cur.close()
+        conn.close()
+
+    # return query data
+    # TODO: return rows to react application for visuals
